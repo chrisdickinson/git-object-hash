b8e93367dd222e2ba49b2a087cbd492be56f2916
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-04-02 18:28:23 -0700

    initial code commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# git-object-hash
+
+turn a [git object](http://npm.im/git-to-js) into a hash!
+
+## api
+
+#### hashify(git object) -> 40 character object hash
+
+## that's it?
+
+> > yep.
+
+> it's pretty tiny.
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,19 @@
+var crypto = require('crypto')
+  , Buffer = require('buffer').Buffer
+
+module.exports = hashify
+
+function hashify(obj) {
+  var buf = obj.serialize()
+    , header
+    
+  header = new Buffer(
+    obj.looseType + ' ' + buf.length + '\0', 'utf8'
+  )
+
+  buf = buf.concat([header, buf], header.length + buf.length)
+  return crypto
+    .createHash('sha1')
+    .update(buf)
+    .digest('hex')  
+}

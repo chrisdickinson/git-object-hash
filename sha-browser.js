e360b1f9905fac4d6eb8ba2a40f12f7a3eea9edc
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-05-04 16:38:05 -0700

    no need for weird coercion anymore

--- sha-browser.js
+++ sha-browser.js
@@ -1,6 +1,5 @@
 var sha1 = require('sha1')
 
 module.exports = function(buf) {
-  var bytes = [].slice.call(buf.parent, buf.offset, buf.offset + buf.length)
-  return sha1(bytes)
+  return sha1(buf)
 }
